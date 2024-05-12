5f4b68e0293d5988cb6b3ffdb7165cf4ce6e861a
Kirill <traineratwot@yandex.ru>
2024-05-12 19:57:41 +0300

    ALL Langs

--- index.ts
+++ index.ts
@@ -14,11 +14,11 @@ console.timeEnd("Optimize images");
 console.time("move images");
 const distImages = new Bun.Glob("./dist/**/*.png");
 for await (const file of distImages.scan(".")) {
-	const name = basename(file);
-	const firstLetter = name[0];
-	const secondLetter = name[1];
-	const dir = join(__dirname, "dist", "images", firstLetter, secondLetter);
-	await mkdir(dir, { recursive: true });
-	rename(file, join(dir, name));
+  const name = basename(file);
+  const firstLetter = name[0];
+  const secondLetter = name[1];
+  const dir = join(__dirname, "dist", "images", firstLetter, secondLetter);
+  await mkdir(dir, { recursive: true });
+  rename(file, join(dir, name));
 }
 console.timeEnd("move images");
