5779caea0b3850314d854ee4b10843f5b0341b02
Aidan647 <kirillkotka@gmail.com>
2024-05-13 18:38:45 +0300

    1

--- index.ts
+++ index.ts
@@ -1,122 +1,131 @@
-import Bun, { $ } from "bun";
-import { copyFile as copy, mkdir, rename, rmdir, writeFile as write } from "node:fs/promises";
-import { basename, join } from "path";
-import type { Device, Devices, Items, OldDevices, ReagentItem, Reagents } from "./tools/types";
+import Bun, { $ } from "bun"
+import { copyFile as copy, mkdir, rename, rmdir, writeFile as write } from "node:fs/promises"
+import { basename, join, dirname } from "path"
+import type { Device, Devices, Items, OldDevice, OldDevices, ReagentItem, Reagents } from "./tools/types"
 
-console.info("Start building...");
+console.info("Start building...")
 
-const GODPromise: Promise<any>[] = [];
+const GODPromise: Promise<any>[] = []
 
-async function step0() {
-	console.time("Clear DIST");
-	await rmdir("./dist", { recursive: true });
-	console.timeEnd("Clear DIST");
+async function clearDist() {
+	console.time("Clear DIST")
+	await rmdir("./dist", { recursive: true })
+	console.timeEnd("Clear DIST")
 }
 
-async function step1() {
-	console.time("Optimize images");
-	await $`tools/oxipng -o max --strip safe --alpha ./images -r -q -t 8 --dir ./dist/images`;
-	console.timeEnd("Optimize images");
+async function optimizeImages() {
+	console.time("Optimize images")
+	await $`tools/oxipng -o max --strip safe --alpha ./source/images -r -q -t 8 --dir ./dist/images`
+	console.timeEnd("Optimize images")
 }
 
-async function step2() {
-	console.time("move images");
-	const distImages = new Bun.Glob("./dist/**/*.png");
+async function moveImages() {
+	console.time("move images")
+	const distImages = new Bun.Glob("./dist/**/*.png")
+	const moves: Promise<void>[] = []
 	for await (const file of distImages.scan(".")) {
-		const name = basename(file);
-		const dir = join(__dirname, "dist", findImage(name));
-		GODPromise.push(moveFile(file, dir));
+		const name = basename(file)
+		const dir = join(__dirname, "dist", findImage(name))
+		moves.push(moveFile(file, dir))
 	}
-	console.timeEnd("move images");
+	await Promise.all(moves)
+	console.timeEnd("move images")
 }
 
-async function step3() {
-	console.time("move other files");
-	await copyFile("./EN/colors.json", "./dist");
-	await copyFile("./EN/consts.json", "./dist");
-	const languages = new Bun.Glob("**/*.json");
-	for await (const file of languages.scan(".")) {
-		const [languages, name] = file.split("/");
-		if (languages.length !== 2) continue;
-		switch (name) {
-			case "constants.json":
-			case "data.json":
-			case "instructions.json":
-				GODPromise.push(copyFile(file, `./dist/${languages}`));
-		}
+async function moveFiles() {
+	console.time("move other files")
+	await copyJSON("source/languages/EN/colors.json", "./dist")
+	await copyJSON("source/languages/EN/consts.json", "./dist")
+	const languages = new Bun.Glob("**/{constants,instructions}.json")
+	for await (const file of languages.scan("source/languages")) {
+		GODPromise.push(copyJSON(join("source", "languages", file), join("dist", dirname(file))))
 	}
-	console.timeEnd("move other files");
+	console.timeEnd("move other files")
+}
+async function moveData() {
+	console.time("move other files")
+	const languages = new Bun.Glob("**/data.json")
+	const moves: Promise<void>[] = []
+	for await (const file of languages.scan("source/languages")) {
+		const sFile = Bun.file(file)
+		const json = (await sFile.json()) as OldDevices
+		GODPromise.push(copyJSON(join("source", "languages", file), join("dist", dirname(file))))
+	}
+	await Promise.all(moves)
+	console.timeEnd("move other files")
 }
 
 async function optimizeData() {
 	//optimize data
 
-	const languages = new Bun.Glob("**/data.json");
+	const languages = new Bun.Glob("**/data.json")
 	for await (const file of languages.scan("./dist")) {
-		console.log(file);
-		const [languages, name] = file.split("/");
-		if (languages.length !== 2) continue;
-		if (name !== "data.json") continue;
-		const data = require(join(__dirname, "dist", file)) as OldDevices;
+		const [languages, name] = [dirname(file), basename(file)]
+		// if (languages.length !== 2) continue
+		if (name !== "data.json") continue
+		console.log(join(__dirname, "dist", file))
+		const sFile = Bun.file(join(__dirname, "dist", file))
+		const data = (await sFile.json()) as OldDevices
+		// const data = require() as OldDevices
 		//OldDevice to Device TODO images
-		const devices: Devices = { data: [], images: {} };
-		const items: Items = { data: [] };
-		const reagents: Reagents = { data: [] };
-		const logics: any = { data: [] };
+		const devices: Devices = { data: [], images: {} }
+		const items: Items = { data: [] }
+		const reagents: Reagents = { data: [] }
+		const logics: any = { data: [] }
 
-		const oldDevices = Object.entries(data);
+		const oldDevices = Object.entries(data)
 		oldDevices
 			.filter(([key, oldDevice]) => {
-				if (oldDevice.tags.includes("item")) return true;
-				if (!oldDevice.PrefabName) return true;
-				if (!oldDevice.MainImage) return true;
-				if (!oldDevice.tags.includes("hasLogic")) return true;
-				let hasChip = false;
-				if (oldDevice.tags.includes("hasChip")) hasChip = true;
+				if (oldDevice.tags.includes("item")) return true
+				if (!oldDevice.PrefabName) return true
+				if (!oldDevice.MainImage) return true
+				if (!oldDevice.tags.includes("hasLogic")) return true
+				let hasChip = false
+				if (oldDevice.tags.includes("hasChip")) hasChip = true
 				const logics: {
-					name: string;
-					permissions: string[];
-				}[] = [];
+					name: string
+					permissions: string[]
+				}[] = []
 				for (const logic of oldDevice.LogicInsert) {
-					const logicName = logic.LogicName.replace(/<[^>]*>?/gm, "");
-					const permissions = logic.LogicAccessTypes.split(" ");
+					const logicName = logic.LogicName.replace(/<[^>]*>?/gm, "")
+					const permissions = logic.LogicAccessTypes.split(" ")
 					logics.push({
 						name: logicName,
 						permissions: permissions,
-					});
+					})
 				}
 				const slots: {
-					SlotName: string;
-					SlotType: string;
-					SlotIndex: number;
-					logic: string[];
-				}[] = [];
-				const slotLogic: Record<number, string[]> = {};
+					SlotName: string
+					SlotType: string
+					SlotIndex: number
+					logic: string[]
+				}[] = []
+				const slotLogic: Record<number, string[]> = {}
 				oldDevice.LogicSlotInsert.forEach((sl) => {
-					const logicName = sl.LogicName.replace(/<[^>]*>?/gm, "");
-					const slotIndexs = sl.LogicAccessTypes.split(", ").map((index) => Number(index));
+					const logicName = sl.LogicName.replace(/<[^>]*>?/gm, "")
+					const slotIndexs = sl.LogicAccessTypes.split(", ").map((index) => Number(index))
 					slotIndexs.forEach((index) => {
-						if (!slotLogic[index]) slotLogic[index] = [];
-						slotLogic[index].push(logicName);
-					});
-				});
+						if (!slotLogic[index]) slotLogic[index] = []
+						slotLogic[index].push(logicName)
+					})
+				})
 				oldDevice.SlotInserts.forEach((slot) => {
-					const slotName = slot.SlotName;
-					const slotType = slot.SlotType;
-					const slotIndex = Number(slot.SlotIndex);
+					const slotName = slot.SlotName
+					const slotType = slot.SlotType
+					const slotIndex = Number(slot.SlotIndex)
 					if (slot.image) {
-						devices.images[`SlotType.${slotType}`] = findImage(slot.image);
+						devices.images[`SlotType.${slotType}`] = findImage(slot.image)
 					}
 					slots.push({
 						SlotName: slotName,
 						SlotType: slotType,
 						SlotIndex: slotIndex,
 						logic: slotLogic[slotIndex] ?? [],
-					});
-				});
+					})
+				})
 				const device: Device = {
 					id: oldDevice.PrefabHash,
-                    Title: oldDevice.Title ?? key,
+					Title: oldDevice.Title ?? key,
 					Key: key,
 					PrefabName: oldDevice.PrefabName,
 					PrefabHash: oldDevice.PrefabHash,
@@ -124,19 +133,21 @@ async function optimizeData() {
 					deviceConnectCount: oldDevice.DeviceConnectCount ?? 0,
 					image: findImage(oldDevice.MainImage),
 					mods: oldDevice.ModeInsert.map((mod) => mod.LogicName),
-					connections: oldDevice.ConnectionInsert.map((connection) => connection.LogicName),
+					connections: oldDevice.ConnectionInsert.map(
+						(connection) => connection.LogicName
+					),
 					slots: slots,
 					tags: oldDevice.tags,
 					logics: logics,
-				};
-				devices.data.push(device);
-				return true;
+				}
+				devices.data.push(device)
+				return true
 			})
 			.filter(([key, oldDevice]) => {
 				//Здесь остались только item
-				if (!oldDevice.tags.includes("item")) return true;
-				if (!oldDevice.MainImage) return true;
-				if (!oldDevice.PrefabName) return true;
+				if (!oldDevice.tags.includes("item")) return true
+				if (!oldDevice.MainImage) return true
+				if (!oldDevice.PrefabName) return true
 
 				items.data.push({
 					id: oldDevice.PrefabHash,
@@ -147,93 +158,106 @@ async function optimizeData() {
 					StackSize: Number(oldDevice.StackSizeText),
 					image: findImage(oldDevice.MainImage),
 					tags: oldDevice.tags,
-				});
+				})
 			})
 			.filter(([key, oldDevice]) => {
 				//Здесь остались только item
-				if (oldDevice.TYPE !== 'reagent') return true;
-				if (!oldDevice.MainImage) return true;
+				if (oldDevice.TYPE !== "reagent") return true
+				if (!oldDevice.MainImage) return true
+				if (!oldDevice.Title) return true
 
-                const reagentItems:ReagentItem[] = [];
+				const reagentItems: ReagentItem[] = []
 
-                oldDevice.FoundInOre.forEach((item) => {
-                    const name = item.NameOfThing.replaceAll(/<[^>]*>?/gm, "")
-                    
-                    const itemData = oldDevices.find(([key, oldDevice]) => oldDevice.Title === name)
-                    if(!itemData) return null
-                    if(!itemData[1].Title) return null
-                    if(!itemData[1].PrefabName) return null
-                    if(!itemData[1].MainImage) return null
-                    if(!itemData[1].PrefabHash) return null
-                    reagentItems.push({
-                        title: itemData[1].Title,
-                        name: itemData[1].PrefabName,
-                        hash: itemData[1].PrefabHash,
-                        count: Number(item.QuantityOfThing) ?? 0,
-                        image: findImage(itemData[1].MainImage)
-                    });
-                })
-                
+				oldDevice.FoundInOre.forEach((item) => {
+					const name = item.NameOfThing.replaceAll(/<[^>]*>?/gm, "")
+
+					const itemData = oldDevices.find(([key, oldDevice]) => oldDevice.Title === name)
+					if (!itemData) return null
+					if (!itemData[1].Title) return null
+					if (!itemData[1].PrefabName) return null
+					if (!itemData[1].MainImage) return null
+					if (!itemData[1].PrefabHash) return null
+					reagentItems.push({
+						title: itemData[1].Title,
+						name: itemData[1].PrefabName,
+						hash: itemData[1].PrefabHash,
+						count: isNaN(Number(item.QuantityOfThing))
+							? 0
+							: Number(item.QuantityOfThing),
+						image: findImage(itemData[1].MainImage),
+					})
+				})
+
 				reagents.data.push({
-					title: oldDevice.Title,
+					title: oldDevice.Title ?? "",
 					name: key,
-					hash: oldDevice.PrefabHash,
+					// hash: oldDevice.PrefabHash,
 					image: findImage(oldDevice.MainImage),
 					items: reagentItems,
-				});
+				})
 			})
 			.filter(([key, oldDevice]) => {
 				//Здесь остались только item
-				if (!oldDevice.Key.startsWith("LogicType")) return true;
+				if (!oldDevice.Key.startsWith("LogicType")) return true
 
-                const reagentItems:ReagentItem[] = [];
-                
+				const reagentItems: ReagentItem[] = []
+
 				logics.data.push({
 					key: key,
 					name: oldDevice.Title,
-					description: oldDevice.Description
-				});
-			});
-		devices.data.sort((a, b) => a.Key.localeCompare(b.Key));
-		items.data.sort((a, b) => a.Key.localeCompare(b.Key));
-		GODPromise.push(writeFile("devices.json", `./dist/${languages}`, devices));
-		GODPromise.push(writeFile("items.json", `./dist/${languages}`, items));
-		GODPromise.push(writeFile("reagents.json", `./dist/${languages}`, reagents));
-		GODPromise.push(writeFile("logics.json", `./dist/${languages}`, logics));
+					description: oldDevice.Description,
+				})
+			})
+		devices.data.sort((a, b) => a.Key.localeCompare(b.Key))
+		items.data.sort((a, b) => a.Key.localeCompare(b.Key))
+		GODPromise.push(writeFile("devices.json", `./dist/${languages}`, devices))
+		GODPromise.push(writeFile("items.json", `./dist/${languages}`, items))
+		GODPromise.push(writeFile("reagents.json", `./dist/${languages}`, reagents))
+		GODPromise.push(writeFile("logics.json", `./dist/${languages}`, logics))
 	}
 }
 
-await step0()
-await step1()
-await step2()
-await step3()
-await optimizeData();
+await clearDist()
+const img = optimizeImages().then(() => moveImages())
+await moveFiles()
+await Promise.all(GODPromise)
+await optimizeData()
 
-await Promise.all(GODPromise);
+await Promise.all(GODPromise)
+await img
 //----------------------------------------------HELPERS----------------------------------------------
-function findImage(fileName: string): string;
-function findImage(fileName: null): null;
+function findImage(fileName: string): string
+function findImage(fileName: null): null
 function findImage(fileName: string | null): string | null {
-	if (!fileName) return null;
-	if (fileName.includes("/") || fileName.includes("\\")) return fileName;
-	const firstLetter = fileName[0];
-	const secondLetter = fileName[1];
-	return join("images", firstLetter, secondLetter, fileName);
+	if (!fileName) return null
+	if (fileName.includes("/") || fileName.includes("\\")) return fileName
+	const firstLetter = fileName[0]
+	const secondLetter = fileName[1]
+	return join("images", firstLetter, secondLetter, fileName)
 }
 
 async function writeFile(file: string, dir: string, content?: {}) {
-	await mkdir(dir, { recursive: true });
+	await mkdir(dir, { recursive: true })
 	return write(join(dir, file), JSON.stringify(content), {
 		encoding: "utf-8",
-	});
+	})
 }
 
 async function moveFile(file: string, dir: string, newName?: string) {
-	await mkdir(dir, { recursive: true });
-	return rename(file, join(dir, newName ?? basename(file)));
+	await mkdir(dir, { recursive: true })
+	return rename(file, join(dir, newName ?? basename(file)))
 }
-
 async function copyFile(file: string, dir: string, newName?: string) {
-	await mkdir(dir, { recursive: true });
-	return copy(file, join(dir, newName ?? basename(file)));
+	await mkdir(dir, { recursive: true })
+	return copy(file, join(dir, newName ?? basename(file)))
+}
+async function copyJSON(file: string, dir: string, newName?: string) {
+	const mk =  mkdir(dir, { recursive: true })
+	const sFile = Bun.file(file)
+	const json = await sFile.json()
+	await mk
+	return write(join(dir, newName ?? basename(file)), JSON.stringify(json), {
+		encoding: "utf-8",
+	})
+	// return copy(file, join(dir, newName ?? basename(file)))
 }
--- tools/types.d.ts
+++ tools/types.d.ts
@@ -5,190 +5,258 @@
 //   const temperatures = Convert.toTemperatures(json);
 
 export type OldDevice = {
-    TYPE:                        string;
-    prefab:                      null | string;
-    Lang:                        string;
-    Key:                         string;
-    Title:                       null | string;
-    Description:                 string;
-    SortPriority:                number;
-    ImportantPage:               boolean;
-    Text:                        string;
-    ConstructWithText:           null;
-    PrefabName:                  null | string;
-    PrefabHash:                  number;
-    PrefabHashString:            null | string;
-    PaintableText:               null | string;
-    StackSizeText:               null | string;
-    ReagentsHash:                number;
-    ReagentsType:                null | string;
-    UnitText:                    null | string;
-    ReagentsText:                null;
-    SpecificHeatText:            null | string;
-    MaxLiquidTemperatureText:    null | string;
-    FreezeTemperatureText:       null | string;
-    BoilingTemperatureText:      null | string;
-    MinLiquidPressure:           null | string;
-    LatentHeatText:              null | string;
-    MolesPerLitreText:           null | string;
-    FlashpointText:              null | string;
-    AutoIgnitionText:            null | string;
-    ConvectionFactorText:        null | string;
-    RadiationFactorText:         null | string;
-    BasePowerDraw:               null | string;
-    MaxPressure:                 null | string;
-    Volume:                      null | string;
-    Nutrition:                   null | string;
-    GrowthTime:                  null | string;
-    PlaceableInRocket:           null | string;
-    RocketMass:                  null | string;
-    RocketEngineForce:           null | string;
-    RocketEngineEfficiency:      null | string;
-    RocketEngineExhaustVelocity: null | string;
-    PressureBreakText:           null | string;
-    CableBreakText:              null | string;
-    InternalAtmosInfoText:       null;
-    DrillHeadProperties:         DrillHeadProperties | null;
-    GasType:                     number;
-    DisplayFilter:               number;
-    CustomSpriteToUse:           null | string;
-    PageCustomCategories:        string[];
-    SlotInserts:                 SlotInsert[];
-    HowToBuild:                  BuildState[];
-    BuildStates:                 BuildState[];
-    StructVersionInsert:         StructVersionInsert[];
-    LogicInsert:                 Insert[];
-    LogicSlotInsert:             Insert[];
-    LogicInstructions:           LogicInstruction[];
-    ModeInsert:                  Insert[];
-    ConnectionInsert:            Insert[];
-    FoundInOre:                  FoundInOre[];
-    FoundInGas:                  FoundInGas[];
-    ConstructedThings:           ConstructedByKit[];
-    ProducedThingsInserts:       ConstructedByKit[];
-    ConstructedByKits:           ConstructedByKit[];
-    ResourcesUsed:               ConstructedByKit[];
-    UsedIn:                      ConstructedByKit[];
-    LifeRequirements:            LifeRequirement[];
-    DeviceConnectCount:          number;
-    MainImage:                   null | string;
-    tags:                        string[];
-    ID:                          string;
+	TYPE: string
+	prefab: null | string
+	Lang: string
+	Key: string
+	Title: null | string
+	Description: string
+	SortPriority: number
+	ImportantPage: boolean
+	Text: string
+	ConstructWithText: null
+	PrefabName: null | string
+	PrefabHash: number
+	PrefabHashString: null | string
+	PaintableText: null | string
+	StackSizeText: null | string
+	ReagentsHash: number
+	ReagentsType: null | string
+	UnitText: null | string
+	ReagentsText: null | string
+	SpecificHeatText: null | string
+	MaxLiquidTemperatureText: null | string
+	FreezeTemperatureText: null | string
+	BoilingTemperatureText: null | string
+	MinLiquidPressure: null | string
+	LatentHeatText: null | string
+	MolesPerLitreText: null | string
+	FlashpointText: null | string
+	AutoIgnitionText: null | string
+	ConvectionFactorText: null | string
+	RadiationFactorText: null | string
+	BasePowerDraw: null | string
+	MaxPressure: null | string
+	Volume: null | string
+	Nutrition: null | string
+	GrowthTime: null | string
+	PlaceableInRocket: null | string
+	RocketMass: null | string
+	RocketEngineForce: null | string
+	RocketEngineEfficiency: null | string
+	RocketEngineExhaustVelocity: null | string
+	PressureBreakText: null | string
+	CableBreakText: null | string
+	InternalAtmosInfoText: null
+	DrillHeadProperties: DrillHeadProperties | null
+	GasType: number
+	DisplayFilter: number
+	CustomSpriteToUse: null | string
+	PageCustomCategories: string[]
+	SlotInserts: SlotInsert[]
+	HowToBuild: BuildState[]
+	BuildStates: BuildState[]
+	StructVersionInsert: StructVersionInsert[]
+	LogicInsert: Insert[]
+	LogicSlotInsert: Insert[]
+	LogicInstructions: LogicInstruction[]
+	ModeInsert: Insert[]
+	ConnectionInsert: Insert[]
+	FoundInOre: FoundInOre[]
+	FoundInGas: FoundInGas[]
+	ConstructedThings: ConstructedByKit[]
+	ProducedThingsInserts: ConstructedByKit[]
+	ConstructedByKits: ConstructedByKit[]
+	ResourcesUsed: ConstructedByKit[]
+	UsedIn: ConstructedByKit[]
+	LifeRequirements: LifeRequirement[]
+	DeviceConnectCount: number
+	MainImage: null | string
+	tags: string[]
+	ID: string
 }
-
+export type NewDevices = {
+	TYPE: string
+	prefab?: string
+	Lang: string
+	Key: string
+	Title?: string
+	Description?: string
+	SortPriority?: number
+	ImportantPage?: boolean
+	Text?: string
+	ConstructWithText?: null
+	PrefabName?: string
+	PrefabHash?: number
+	PrefabHashString?: string
+	PaintableText?: string
+	StackSizeText?: string
+	ReagentsHash?: number
+	ReagentsType?: string
+	UnitText?: string
+	ReagentsText?: null
+	SpecificHeatText?: string
+	MaxLiquidTemperatureText?: string
+	FreezeTemperatureText?: string
+	BoilingTemperatureText?: string
+	MinLiquidPressure?: string
+	LatentHeatText?: string
+	MolesPerLitreText?: string
+	FlashpointText?: string
+	AutoIgnitionText?: string
+	ConvectionFactorText?: string
+	RadiationFactorText?: string
+	BasePowerDraw?: string
+	MaxPressure?: string
+	Volume?: string
+	Nutrition?: string
+	GrowthTime?: string
+	PlaceableInRocket?: string
+	RocketMass?: string
+	RocketEngineForce?: string
+	RocketEngineEfficiency?: string
+	RocketEngineExhaustVelocity?: string
+	PressureBreakText?: string
+	CableBreakText?: string
+	InternalAtmosInfoText?: null
+	DrillHeadProperties?: DrillHeadProperties | null
+	GasType?: number
+	DisplayFilter?: number
+	CustomSpriteToUse?: string
+	PageCustomCategories?: string[]
+	SlotInserts?: SlotInsert[]
+	HowToBuild?: BuildState[]
+	BuildStates?: BuildState[]
+	StructVersionInsert?: StructVersionInsert[]
+	LogicInsert?: Insert[]
+	LogicSlotInsert?: Insert[]
+	LogicInstructions?: LogicInstruction[]
+	ModeInsert?: Insert[]
+	ConnectionInsert?: Insert[]
+	FoundInOre?: FoundInOre[]
+	FoundInGas?: FoundInGas[]
+	ConstructedThings?: ConstructedByKit[]
+	ProducedThingsInserts?: ConstructedByKit[]
+	ConstructedByKits?: ConstructedByKit[]
+	ResourcesUsed?: ConstructedByKit[]
+	UsedIn?: ConstructedByKit[]
+	LifeRequirements?: LifeRequirement[]
+	DeviceConnectCount?: number
+	MainImage?: string
+	tags?: string[]
+	ID: string
+}
 export type BuildState = {
-    PrinterName: string;
-    TierName:    null | string;
-    Details:     null | string;
-    Description: string;
-    PageLink:    null | string;
-    image:       null | string;
+	PrinterName: string
+	TierName: null | string
+	Details: null | string
+	Description: string
+	PageLink: null | string
+	image: null | string
 }
 
 export type Insert = {
-    LogicName:        string;
-    LogicAccessTypes: string;
+	LogicName: string
+	LogicAccessTypes: string
 }
 
 export type ConstructedByKit = {
-    NameOfThing: string;
-    PrefabHash:  number;
-    PageLink:    string;
-    image:       null | string;
+	NameOfThing: string
+	PrefabHash: number
+	PageLink: string
+	image: null | string
 }
 
 export type DrillHeadProperties = {
-    SpeedMultiplier:            string;
-    ReagentYieldMultiplier:     string;
-    IceYieldMultiplier:         string;
-    HealthMultiplier:           string;
-    PowerConsumptionMultiplier: string;
+	SpeedMultiplier: string
+	ReagentYieldMultiplier: string
+	IceYieldMultiplier: string
+	HealthMultiplier: string
+	PowerConsumptionMultiplier: string
 }
 
 export type FoundInGas = {
-    NameOfThing:     string;
-    QuantityOfThing: string;
+	NameOfThing: string
+	QuantityOfThing: string
 }
 
 export type FoundInOre = {
-    NameOfThing:     string;
-    QuantityOfThing: number | string;
+	NameOfThing: string
+	QuantityOfThing: number | string
 }
 
 export type LifeRequirement = {
-    Name:      string;
-    Value:     string;
-    Gene:      string;
-    ValueSize: number;
+	Name: string
+	Value: string
+	Gene: string
+	ValueSize: number
 }
 
 export type LogicInstruction = {
-    Text:  string;
-    Index: number;
-    Info:  string;
+	Text: string
+	Index: number
+	Info: string
 }
 
 export type SlotInsert = {
-    SlotName:  string;
-    SlotType:  string;
-    SlotIndex: string;
-    image:     null | string;
+	SlotName: string
+	SlotType: string
+	SlotIndex: string
+	image: null | string
 }
 
 export type StructVersionInsert = {
-    StructureVersion:       string;
-    CreationMultiplier:     string;
-    EnergyCostMultiplier:   string;
-    MaterialCostMultiplier: string;
-    BuildTimeMultiplier:    string;
-    image:                  null;
+	StructureVersion: string
+	CreationMultiplier: string
+	EnergyCostMultiplier: string
+	MaterialCostMultiplier: string
+	BuildTimeMultiplier: string
+	image: null
 }
 
 export type OldDevices = Record<string, OldDevice>
 export type Device = {
-    id: number,
-    Title: string,
-    Key: string,
-    PrefabName: string,
-    PrefabHash: number,
-    hasChip: boolean,
-    deviceConnectCount: number,
-    image: string,
-    mods: string[],
-    connections: string[],
-    slots: {
-        SlotName: string,
-        SlotType: string,
-        SlotIndex: number,
-        logic: string[]
-    }[],
-    tags: string[],
-    logics: {
-        name: string,
-        permissions: string[]
-    }[]
+	id: number
+	Title: string
+	Key: string
+	PrefabName: string
+	PrefabHash: number
+	hasChip: boolean
+	deviceConnectCount: number
+	image: string
+	mods: string[]
+	connections: string[]
+	slots: {
+		SlotName: string
+		SlotType: string
+		SlotIndex: number
+		logic: string[]
+	}[]
+	tags: string[]
+	logics: {
+		name: string
+		permissions: string[]
+	}[]
 }
 
 export type Item = {
-    id:         number;
-    Title:        string;
-    Key:        string;
-    PrefabName: string;
-    PrefabHash: number;
-    StackSize:  number;
-    image:      string;
-    tags:       string[];
+	id: number
+	Title: string
+	Key: string
+	PrefabName: string
+	PrefabHash: number
+	StackSize: number
+	image: string
+	tags: string[]
 }
-
 
 export type Devices = {
-    data: Device[]
-    images:Record<`SlotType.${string}`, string>
+	data: Device[]
+	images: Record<`SlotType.${string}`, string>
 }
 export type Items = {
-    data: Item[]
+	data: Item[]
 }
-
 
 // To parse this data:
 //
@@ -197,20 +265,21 @@ export type Items = {
 //   const reagents = Convert.toReagents(json);
 
 export type Reagents = {
-    data: Reagent[];
+	data: Reagent[]
 }
 
-export type Datum = {
-    name:  string;
-    hash:  number;
-    image: string;
-    items: ReagentItem[];
+export type Reagent = {
+	title: string
+	name: string
+	// hash:  number;
+	image: string
+	items: ReagentItem[]
 }
 
 export type ReagentItem = {
-    title:  string;
-    name:  string;
-    hash:  number;
-    image: string;
-    count: number;
+	title: string
+	name: string
+	hash: number
+	image: string
+	count: number
 }
