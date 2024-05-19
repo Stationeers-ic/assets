2cd407da5513020d5541f0d229d5e417a4a3d958
kirill <traineratwot@yandex.ru>
2024-05-19 20:26:24 +0300

    chore: Update constants and optimize data generation

--- index.ts
+++ index.ts
@@ -1,21 +1,21 @@
 import Bun, { $ } from "bun";
 import {
-  copyFile as copy,
-  mkdir,
-  rename,
-  rmdir,
-  writeFile as write,
+	copyFile as copy,
+	mkdir,
+	rename,
+	rmdir,
+	writeFile as write,
 } from "node:fs/promises";
 import { cpus } from "node:os";
 import { basename, dirname, join } from "path";
 import { dir_index, walkDir } from "./tools/dir_index";
 import type {
-  Device,
-  Devices,
-  Items,
-  OldDevices,
-  ReagentItem,
-  Reagents,
+	Device,
+	Devices,
+	Items,
+	OldDevices,
+	ReagentItem,
+	Reagents,
 } from "./tools/types";
 
 console.info("Start building...");
@@ -108,144 +108,142 @@ async function optimizeData() {
       const logics: any = { data: [] };
 
       const oldDevices = Object.entries(data);
-      oldDevices
-        .filter(([key, oldDevice]) => {
-          if (oldDevice.tags.includes("item")) return true;
-          if (!oldDevice.PrefabName) return true;
-          if (!oldDevice.MainImage) return true;
-          if (!oldDevice.tags.includes("hasLogic")) return true;
-          let hasChip = false;
-          if (oldDevice.tags.includes("hasChip")) hasChip = true;
-          const logics: {
-            name: string;
-            permissions: string[];
-          }[] = [];
-          for (const logic of oldDevice?.LogicInsert) {
-            const logicName = logic.LogicName.replace(/<[^>]*>?/gm, "");
-            const permissions = logic.LogicAccessTypes.split(" ");
-            logics.push({
-              name: logicName,
-              permissions: permissions,
-            });
-          }
-          const slots: {
-            SlotName: string;
-            SlotType: string;
-            SlotIndex: number;
-            logic: string[];
-          }[] = [];
-          const slotLogic: Record<number, string[]> = {};
-          oldDevice?.LogicSlotInsert?.forEach((sl) => {
-            const logicName = sl.LogicName.replace(/<[^>]*>?/gm, "");
-            const slotIndexs = sl.LogicAccessTypes.split(", ").map((index) =>
-              Number(index),
-            );
-            slotIndexs.forEach((index) => {
-              if (!slotLogic[index]) slotLogic[index] = [];
-              slotLogic[index].push(logicName);
-            });
+      oldDevices.forEach(([key, oldDevice]) => {
+        if (!oldDevice.PrefabName) return true;
+        if (!oldDevice.MainImage) return true;
+        if (!oldDevice.tags.includes("hasLogic")) return true;
+        let hasChip = false;
+        if (oldDevice.tags.includes("hasChip")) hasChip = true;
+        const logics: {
+          name: string;
+          permissions: string[];
+        }[] = [];
+        for (const logic of oldDevice?.LogicInsert) {
+          const logicName = logic.LogicName.replace(/<[^>]*>?/gm, "");
+          const permissions = logic.LogicAccessTypes.split(" ");
+          logics.push({
+            name: logicName,
+            permissions: permissions,
           });
-          oldDevice?.SlotInserts?.forEach((slot) => {
-            const slotName = slot.SlotName;
-            const slotType = slot.SlotType;
-            const slotIndex = Number(slot.SlotIndex);
-            if (slot.image) {
-              devices.images[`SlotType.${slotType}`] = findImage(slot.image);
-            }
-            slots.push({
-              SlotName: slotName,
-              SlotType: slotType,
-              SlotIndex: slotIndex,
-              logic: slotLogic[slotIndex] ?? [],
-            });
-          });
-          const device: Device = {
-            id: oldDevice?.PrefabHash,
-            Title: oldDevice?.Title ?? key,
-            Key: key,
-            PrefabName: oldDevice?.PrefabName,
-            PrefabHash: oldDevice?.PrefabHash,
-            hasChip: hasChip,
-            deviceConnectCount: oldDevice.DeviceConnectCount ?? 0,
-            image: findImage(oldDevice.MainImage),
-            mods: oldDevice?.ModeInsert?.map((mod) => mod.LogicName),
-            connections: oldDevice?.ConnectionInsert?.map(
-              (connection) => connection?.LogicName,
-            ),
-            slots: slots,
-            tags: oldDevice?.tags,
-            logics: logics,
-          };
-          devices.data.push(device);
-          return true;
-        })
-        .filter(([key, oldDevice]) => {
-          //Здесь остались только item
-          if (!oldDevice.tags.includes("item")) return true;
-          if (!oldDevice.MainImage) return true;
-          if (!oldDevice.PrefabName) return true;
-
-          items.data.push({
-            id: oldDevice.PrefabHash,
-            Title: oldDevice.Title ?? key,
-            Key: key,
-            PrefabName: oldDevice.PrefabName,
-            PrefabHash: oldDevice.PrefabHash,
-            StackSize: Number(oldDevice.StackSizeText),
-            image: findImage(oldDevice.MainImage),
-            tags: oldDevice.tags,
-          });
-        })
-        .filter(([key, oldDevice]) => {
-          //Здесь остались только item
-          if (oldDevice.TYPE !== "reagent") return true;
-          if (!oldDevice.MainImage) return true;
-          if (!oldDevice.Title) return true;
-
-          const reagentItems: ReagentItem[] = [];
-
-          oldDevice.FoundInOre.forEach((item) => {
-            const name = item.NameOfThing.replaceAll(/<[^>]*>?/gm, "");
-
-            const itemData = oldDevices.find(
-              ([key, oldDevice]) => oldDevice.Title === name,
-            );
-            if (!itemData) return null;
-            if (!itemData[1].Title) return null;
-            if (!itemData[1].PrefabName) return null;
-            if (!itemData[1].MainImage) return null;
-            if (!itemData[1].PrefabHash) return null;
-            reagentItems.push({
-              title: itemData[1].Title,
-              name: itemData[1].PrefabName,
-              hash: itemData[1].PrefabHash,
-              count: isNaN(Number(item.QuantityOfThing))
-                ? 0
-                : Number(item.QuantityOfThing),
-              image: findImage(itemData[1].MainImage),
-            });
-          });
-
-          reagents.data.push({
-            title: oldDevice.Title ?? "",
-            name: key,
-            hash: oldDevice.ReagentsHash ?? oldDevice.PrefabHash,
-            image: findImage(oldDevice.MainImage),
-            items: reagentItems,
-          });
-        })
-        .filter(([key, oldDevice]) => {
-          //Здесь остались только item
-          if (!oldDevice.Key.startsWith("LogicType")) return true;
-
-          const reagentItems: ReagentItem[] = [];
-
-          logics.data.push({
-            key: key,
-            name: oldDevice.Title,
-            description: oldDevice.Description,
+        }
+        const slots: {
+          SlotName: string;
+          SlotType: string;
+          SlotIndex: number;
+          logic: string[];
+        }[] = [];
+        const slotLogic: Record<number, string[]> = {};
+        oldDevice?.LogicSlotInsert?.forEach((sl) => {
+          const logicName = sl.LogicName.replace(/<[^>]*>?/gm, "");
+          const slotIndexs = sl.LogicAccessTypes.split(", ").map((index) =>
+            Number(index),
+          );
+          slotIndexs.forEach((index) => {
+            if (!slotLogic[index]) slotLogic[index] = [];
+            slotLogic[index].push(logicName);
           });
         });
+        oldDevice?.SlotInserts?.forEach((slot) => {
+          const slotName = slot.SlotName;
+          const slotType = slot.SlotType;
+          const slotIndex = Number(slot.SlotIndex);
+          if (slot.image) {
+            devices.images[`SlotType.${slotType}`] = findImage(slot.image);
+          }
+          slots.push({
+            SlotName: slotName,
+            SlotType: slotType,
+            SlotIndex: slotIndex,
+            logic: slotLogic[slotIndex] ?? [],
+          });
+        });
+        const device: Device = {
+          id: oldDevice?.PrefabHash,
+          Title: oldDevice?.Title ?? key,
+          Key: key,
+          PrefabName: oldDevice?.PrefabName,
+          PrefabHash: oldDevice?.PrefabHash,
+          hasChip: hasChip,
+          deviceConnectCount: oldDevice.DeviceConnectCount ?? 0,
+          image: findImage(oldDevice.MainImage),
+          mods: oldDevice?.ModeInsert?.map((mod) => mod.LogicName),
+          connections: oldDevice?.ConnectionInsert?.map(
+            (connection) => connection?.LogicName,
+          ),
+          slots: slots,
+          tags: oldDevice?.tags,
+          logics: logics,
+        };
+        devices.data.push(device);
+        return true;
+      });
+      oldDevices.forEach(([key, oldDevice]) => {
+        //Здесь остались только item
+        if (!oldDevice.tags.includes("item")) return true;
+        if (!oldDevice.MainImage) return true;
+        if (!oldDevice.PrefabName) return true;
+
+        items.data.push({
+          id: oldDevice.PrefabHash,
+          Title: oldDevice.Title ?? key,
+          Key: key,
+          PrefabName: oldDevice.PrefabName,
+          PrefabHash: oldDevice.PrefabHash,
+          StackSize: Number(oldDevice.StackSizeText),
+          image: findImage(oldDevice.MainImage),
+          tags: oldDevice.tags,
+        });
+      });
+      oldDevices.forEach(([key, oldDevice]) => {
+        //Здесь остались только item
+        if (oldDevice.TYPE !== "reagent") return true;
+        if (!oldDevice.MainImage) return true;
+        if (!oldDevice.Title) return true;
+
+        const reagentItems: ReagentItem[] = [];
+
+        oldDevice.FoundInOre.forEach((item) => {
+          const name = item.NameOfThing.replaceAll(/<[^>]*>?/gm, "");
+
+          const itemData = oldDevices.find(
+            ([key, oldDevice]) => oldDevice.Title === name,
+          );
+          if (!itemData) return null;
+          if (!itemData[1].Title) return null;
+          if (!itemData[1].PrefabName) return null;
+          if (!itemData[1].MainImage) return null;
+          if (!itemData[1].PrefabHash) return null;
+          reagentItems.push({
+            title: itemData[1].Title,
+            name: itemData[1].PrefabName,
+            hash: itemData[1].PrefabHash,
+            count: isNaN(Number(item.QuantityOfThing))
+              ? 0
+              : Number(item.QuantityOfThing),
+            image: findImage(itemData[1].MainImage),
+          });
+        });
+
+        reagents.data.push({
+          title: oldDevice.Title ?? "",
+          name: key,
+          hash: oldDevice.ReagentsHash ?? oldDevice.PrefabHash,
+          image: findImage(oldDevice.MainImage),
+          items: reagentItems,
+        });
+      });
+      oldDevices.forEach(([key, oldDevice]) => {
+        //Здесь остались только item
+        if (!oldDevice.Key.startsWith("LogicType")) return true;
+
+        const reagentItems: ReagentItem[] = [];
+
+        logics.data.push({
+          key: key,
+          name: oldDevice.Title,
+          description: oldDevice.Description,
+        });
+      });
       devices.data.sort((a, b) => a.Key.localeCompare(b.Key));
       items.data.sort((a, b) => a.Key.localeCompare(b.Key));
       await writeFile(
@@ -287,7 +285,7 @@ async function generateIndex() {
 }
 
 await clearDist();
-GODPromise.push(optimizeImages().then(() => moveImages()));
+// GODPromise.push(optimizeImages().then(() => moveImages()));
 GODPromise.push(moveFiles());
 GODPromise.push(moveData());
 await Promise.all(GODPromise);
